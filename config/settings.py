f4ebee379d8344bced5e4d9917119ee3364201e1
Mahdi <mmf1344py@gmail.com>
2023-03-02 22:27:13 +0330

    create CustomUser model in accounts app, create superuser

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # third-party apps
+
+    # local apps
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +119,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CustomUser config
+AUTH_USER_MODEL = "accounts.CustomUser"
